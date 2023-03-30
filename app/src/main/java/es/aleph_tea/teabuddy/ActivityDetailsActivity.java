07b7e4c56210ab3e1d744227585c295f05b1d400
IsmaelRobledoFernandez <ismael.robledo.fernandez@alumnos.upm.es>
2023-03-30 19:00:42 +0200

    Añadir primera version lista de actividades

---
+++ app/src/main/java/es/aleph_tea/teabuddy/ActivityDetailsActivity.java
@@ -0,0 +1,32 @@
+package es.aleph_tea.teabuddy;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import java.util.ArrayList;
+
+import es.aleph_tea.teabuddy.databinding.ActivityDetailsBinding;
+
+public class ActivityDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+
+}
--- app/src/main/java/es/aleph_tea/teabuddy/UnoFragment.java
+++ app/src/main/java/es/aleph_tea/teabuddy/UnoFragment.java
@@ -1,19 +1,30 @@
 package es.aleph_tea.teabuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
+import android.text.style.TabStopSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link UnoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class UnoFragment extends Fragment {
+public class UnoFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,6 +34,12 @@ public class UnoFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private ListView mListView;
+
+    private List<String> actividades;
+
+    private ArrayAdapter<String> mAdapter;
 
     public UnoFragment() {
         // Required empty public constructor
@@ -50,11 +67,32 @@ public class UnoFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+        Toast.makeText(getContext(),"Elemento clickado: " + position, Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(getActivity().getApplicationContext(), ActivityDetailsActivity.class));
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflar el layout para este fragmento
         return inflater.inflate(R.layout.fragment_uno, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Inicializacion de la ListView, la lista de actividades y el adapter
+        mListView = (ListView)view.findViewById(R.id.listView);
+        actividades = new ArrayList<>();
+
+        actividades.add("Brian");
+        actividades.add("Raquel");
+        actividades.add("Ismael");
+
+        mAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1,actividades);
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(this);
     }
 }
